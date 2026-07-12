44203936478b0ab4af40ee555a41687d6bf1ee49
Gulmohar <92192914+vvihar@users.noreply.github.com>
2026-07-12 14:25:28 +0900

    Cloudflare用の仮想モジュール解決を生成物に寄せる

--- src/__mocks__/app-server-dummy.ts
+++
@@ -1,3 +0,0 @@
-export const getRequestEvent = () => {
-	throw new Error('$app/server is not available in Durable Object context');
-};
--- src/__mocks__/sveltekit-worker.ts
+++
@@ -1,3 +0,0 @@
-export default {
-	fetch: async () => new Response('sveltekit-worker mock', { status: 200 })
-};
